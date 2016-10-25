988539a65f5fadd366d99755e9c64dce44dbea55
Duy Khoa <duykhoa12t@gmail.com>
2016-10-25 11:07:59 +0800

    fix bug

--- e1.rb
+++ e1.rb
@@ -4,26 +4,18 @@ class E1Test < Minitest::Test
   def amuse(arr)
     mused = [1] * arr.size
 
-    i = 0
+    (0..arr.size).each do |k|
+      (0...arr.size).each do |i|
+        (0...arr.size).each do |j|
+          next unless j != i
 
-    while i < arr.size
-      j = 0
-
-      while (j < arr.size)
-        if j != i
           if arr[i] > arr[j] && !(mused[i] > mused[j])
             mused[i] += 1
-          end
-
-          if arr[i] < arr[j] && !(mused[i] < mused[j])
+          elsif arr[i] < arr[j] && !(mused[i] < mused[j])
             mused[j] += 1
           end
         end
-
-        j += 1
       end
-
-      i += 1
     end
 
     mused
@@ -42,6 +34,8 @@ class E1Test < Minitest::Test
     assert_equal([1, 3, 2], amuse([1, 3, 2]))
     assert_equal([2, 1, 1], amuse([3, 1, 1]))
     assert_equal([2, 1, 2], amuse([3, 1, 3]))
+    assert_equal([3, 2, 1], amuse([3, 2, 1]))
+
     assert_equal([1, 2, 1], amuse([1, 3, 1]))
     assert_equal([2, 2, 1], amuse([3, 3, 1]))
     assert_equal([2, 2, 1], amuse([3, 3, 1]))
@@ -52,5 +46,7 @@ class E1Test < Minitest::Test
     assert_equal([1, 3, 1, 2], amuse([1, 99, 1, 5]))
     assert_equal([1, 3, 4, 2], amuse([1, 99, 100, 5]))
     assert_equal([2, 3, 4, 1], amuse([88, 99, 100, 5]))
+    assert_equal([2, 3, 3, 1], amuse([88, 100, 100, 5]))
+    assert_equal([4, 3, 2, 1], amuse([101, 99, 98, 5]))
   end
 end
